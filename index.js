b8c315c6034bd8f4c1543cdab276f352aba3afae
1sGu7 <22dh110298@st.huflit.edu.vn>
2025-06-05 10:03:02 +0700

    Fix bug can deploy

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Lấy thông tin kết nối từ biến môi trường
+// Lấy chuỗi kết nối từ biến môi trường
+const connectionString = process.env.DATABASE_URL || 'postgresql://root:TNdT7Ds6p8Tog5tHNOcjYe3Y5ICfr5Up@dpg-d10gd463jp1c738ve1h0-a.singapore-postgres.render.com/test_database_r0so';
+
 const pool = new Pool({
-  connectionString: postgresql://root:TNdT7Ds6p8Tog5tHNOcjYe3Y5ICfr5Up@dpg-d10gd463jp1c738ve1h0-a/test_database_r0so,
+  connectionString: connectionString,  // Sử dụng chuỗi kết nối
   ssl: {
     rejectUnauthorized: false,  // Cấu hình SSL nếu Render yêu cầu
   }
@@ -13,9 +15,10 @@ const pool = new Pool({
 
 app.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT NOW()');  // Lấy thời gian từ DB để kiểm tra kết nối
+    const result = await pool.query('SELECT NOW()');  // Kiểm tra kết nối với DB
     res.send(`Database connected at ${result.rows[0].now}`);
   } catch (err) {
+    console.error('Error connecting to database', err);
     res.status(500).send('Error connecting to database');
   }
 });
